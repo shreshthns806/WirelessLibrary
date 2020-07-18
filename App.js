bf84f4667104c330fb47a7fde597ab8b4c277d84
shreshthns806 <shreshthns.425@gmail.com>
2020-07-18 11:50:31 +0530

    C-68 CaseStudy

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import TransactionScreen from './screens/BookTransaction';
+import SearchScreen from './screens/SearchScreen';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component{
+  render(){
+    return (
+      <AppContainer />
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -19,3 +21,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const tabNavigator = createBottomTabNavigator({
+  Transaction:{screen:TransactionScreen},
+  Search:{screen:SearchScreen},
+})
+
+const AppContainer = createAppContainer(tabNavigator)
